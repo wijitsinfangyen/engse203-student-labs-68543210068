24aeed98acd98ecc7c15b66eabc9af22970da687
wijitsinfangyen <wijitsinfangyen@gmail.com>
2026-07-23 14:36:07 +0700

    feat(week-01): migrate developer environment lab

---
+++ labs/week-01/source/src/hello.js
@@ -0,0 +1,12 @@
+const student = {
+  name: "Wijitsin Fangyen",
+  studentId: "68543210068-1",
+  os: process.platform,
+  node: process.version,
+};
+
+function createGreeting({ name, studentId, os, node }) {
+  return `Hello ${name} (${studentId}) | OS: ${os} | Node: ${node}`;
+}
+
+console.log(createGreeting(student));
